09640cbbf5adcbe93eab81a217a3b02e17f951a5
Keerthivasan D <keerthivasan.d@thoughtworks.com>
2022-06-20 08:44:47 +0530

    [TODO - Frontend] Added image assets and font package

--- todo-appliccation/frontend/src/App.js
+++ todo-appliccation/frontend/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";  
+import React from "react";
+import "./assets/css/App.css";
 
 const App = () => {
-	return (
-		<div>
-			<h1>Hello Folks!</h1>
-		</div>
-	);
+    return (
+        <div className="app">
+            <h1>Hello Folks!</h1>
+        </div>
+    );
 };
 
 export default App;
